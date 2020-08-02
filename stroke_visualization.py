baa50f10423216d03cb60b02736983839e841b5c
Carlos <carlos.cruzper@gmail.com>
2020-08-02 19:24:01 +0200

    changed structure in folders

--- stroke_visualization.py
+++ stroke_visualization.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
 
 import streamlit as st
 import numpy as np
@@ -9,7 +5,6 @@ import pandas as pd
 import pickle
 #from PIL import Image
 
-# In[ ]:
 st.title('Get to know your heart')
 st.subheader('Predict your heart status')
 
@@ -19,22 +14,11 @@ st.sidebar.title('Set parameters')
 
 #Default paramenters of a healthy patient
 ageInit= 45
-'''heightInit= 165
-weightInit= 65'''
 sPressureInit= 120
 dPressureInit= 80
 cholesterolInit= 1
 
-
 age = st.sidebar.number_input('Age', value=ageInit)
-
-'''sexDict = {'Male':1, 'Female':2}
-sex = st.sidebar.selectbox('Sex', ['Male','Female'])
-sex = sexDict[sex]
-
-height = st.sidebar.number_input('Height (cm)', value=heightInit)
-
-weight = st.sidebar.number_input('Weight (Kg)', value=weightInit)'''
 
 sPressure = st.sidebar.number_input('Systolic Pressure',value=sPressureInit)
 
@@ -45,15 +29,6 @@ pulse = sPressure-dPressure
 cholesterolDict = {'Low: <200 mg/dL':1, 'High: 200-240 mg/dL':2, 'Very high: >240 mg/dL':3}
 cholesterol = st.sidebar.selectbox('Cholresterol', ['Low: <200 mg/dL', 'High: 200-240 mg/dL', 'Very high: >240 mg/dL'])
 cholesterol = cholesterolDict[cholesterol]
-
-'''diabetes = st.sidebar.checkbox('Diabetes')
-
-smoker = st.sidebar.checkbox('Smoker')
-
-alcoholic = st.sidebar.checkbox('Alcoholic')
-
-sport = st.sidebar.checkbox('Active Sport')'''
-
 
 inputData = [age, sPressure,
        dPressure, cholesterol, pulse]
